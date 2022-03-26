b0d62ad7a41ede2722d3ac80d9cfbdd2341745c2
DevSPCBot <96945769+DevSPCBot@users.noreply.github.com>
2022-03-26 20:38:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+function p() {
+    // document.getElementById("periode").innerHTML = `<h1 class="periodep"></h1>`
+    document.getElementById("periode").innerHTML = `<h1 class="periodep">${new Intl.DateTimeFormat("id-ID", {month: "long", year: "numeric"}).format(new Date(new Date().getFullYear(), new Date().getMonth() -1)).toUpperCase()}</h1>`
+}
+function showTopvoter() {
+    fetch(process.env.API)
+    .then((res) => res.json())
+    .then(data => {
+        var date = new Date();
+        let currentMonth = date.getMonth() + 1;
+        let previousMonth = date.getMonth()
+        let year = date.getFullYear();
+        let daysPreviousMonth = new Date(year, previousMonth, 0).getDate();
+        let daysCurrentMonth = new Date(year, currentMonth, 0).getDate();
+        let str = ''
+        for (const top of data.voters.filter(p => p.votes == daysPreviousMonth)) {
+            let list = document.getElementById("list")
+            str += `<tr><td>${top.nickname}</td><td>${top.votes}</td></tr>`
+            
+            list.innerHTML = str;
+        }
+    })
+
+        
+
+}
+function experiment() {
+    // let tanggal = new Intl.DateTimeFormat("id-ID", {day: "numeric"}).format(new Date())
+
+    console.log(daysInMonth)
+    // console.log(month)
+    console.log(year)
+}
+    p()
+    // experiment()
+
+    showTopvoter()
+
+    document.querySelector('button').addEventListener('click', function() {
+        html2canvas(document.querySelector('.output'), {
+            onrendered: function(canvas) {
+            // document.body.appendChild(canvas);
+            // let url = document.createElement('a')
+
+            return Canvas2Image.saveAsPNG(canvas);
+          }
+        });
+      });
+
+// document.getElementById("downloadbutton").addEventListener("click", 
+//     html2canvas(document.body), {
+//         onrendered: function(canvas) {
+//             document.body.append(canvas)
+//         }
+//     })
+
+
+    
